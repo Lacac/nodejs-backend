4706fd6e23415c1dbd3f38fc4eca5bca49184d8d
Laclac <ngochths150417@fpt.eedu.vn>
2023-06-07 15:16:26 +0700

    #18-Nodejs-MVC

--- src/route/web.js
+++ src/route/web.js
@@ -1,12 +1,16 @@
 import express from "express";
-
+import homeController from "../controllers/homeController"
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get("/",(req,res)=> {
-        return res.send('Hello world with Ngocht')
+    router.get("/", homeController.getHomePage);
+    router.get("/about", homeController.getAboutPage);
+
+    router.get("/hoidanit", (req, res) => {
+        return res.send(' Hello world with Hoi dan IT')
     });
-    return app.use("/",router);
+
+    return app.use("/", router);
 }
 
 module.exports = initWebRoutes
